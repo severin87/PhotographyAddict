3fd9dd23e43c6ffb201e9b34c97589757fd9a576
severin87 <severin0887@outlook.com>
2019-03-06 13:50:23 +0200

    Create default ASP.net MVC application.

---
+++ source/PhotographyAddicted.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PhotographyAddicted.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
