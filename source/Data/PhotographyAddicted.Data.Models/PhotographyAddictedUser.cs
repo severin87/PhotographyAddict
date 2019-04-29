bec27d2104449d2e30ec606f72ced0a8e5636038
severin87 <severin0887@outlook.com>
2019-04-30 00:02:56 +0300

    Prepare Image preview.

--- source/Data/PhotographyAddicted.Data.Models/PhotographyAddictedUser.cs
+++ source/Data/PhotographyAddicted.Data.Models/PhotographyAddictedUser.cs
@@ -79,6 +79,7 @@ namespace PhotographyAddicted.Web.Areas.Identity.Data
 
         //TODO: Nastroika dali sa syglasni s pravilata
 
+        //TODO: Da sloja snimka na usarite bez profilni snimki.
         //TODO: Proveri vsichki Contrroleri s action Preview dali imat zashtita on Null stoinosti.
         //TODO Chasa da se opravi.
         //TODO RAzchisti HomeControlera.
