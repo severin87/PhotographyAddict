8b7933c2dced4848942d5bee72db076adea3d85e
severin87 <severin0887@outlook.com>
2019-05-06 21:20:41 +0300

    Remove Renamer.

--- source/Web/PhotographyAddicted.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ source/Web/PhotographyAddicted.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -81,7 +81,7 @@ namespace PhotographyAddicted.Web.Areas.Identity.Pages.Account
             returnUrl = returnUrl ?? Url.Content("~/");
             if (ModelState.IsValid)
             {
-                var user = new PhotographyAddictedUser { UserName = Input.UserName, LastLogin = Input.LastLogin, Email = Input.Email, CreationDate = Input.CreationDate, };
+                var user = new PhotographyAddictedUser { UserName = Input.UserName, LastLogin = Input.LastLogin, Email = Input.Email, CreationDate = Input.CreationDate };
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
